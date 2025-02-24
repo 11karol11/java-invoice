d906a27507e0b775cd2efddacbd83d8c0a845f3b
Karol Filipek <karolfilipek11@gmail.com>
2025-02-24 19:16:36 +0100

    Add two test methods implementation of the program in invoice file

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,27 +1,50 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     private Collection<Product> products;
-
+    public Invoice() {
+        products = new ArrayList<>();
+    }
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than zero");
+        }
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        }
+        for (int i = 0; i < quantity; i++) {
+            products.add(product);
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Product product : products) {
+            subtotal = subtotal.add(product.getPrice());
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal taxAmount = BigDecimal.ZERO;
+        for (Product product : products) {
+            BigDecimal taxForProduct = product.getPrice().multiply(product.getTaxPercent());
+            taxAmount = taxAmount.add(taxForProduct);
+        }
+        return taxAmount;
     }
 
     public BigDecimal getTotal() {
